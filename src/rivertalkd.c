dc8e85cf1d513d524cf96797022399940d545bcf
doooley <dooleyct@tcd.ie>
2014-05-12 21:11:50 +0000

    uses select now, can connect to loads of ppl

--- src/rivertalkd.c
+++ src/rivertalkd.c
@@ -9,120 +9,127 @@
 #include <netdb.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include "wlog.h"
+//#include "wlog.h"
 
 #define MAXDATASIZE 140
 #define PORT "4991"
 #define BACKLOG 10
 
-void sigchld_handler(int s) {
-  while(waitpid(-1, NULL, WNOHANG) > 0);
+int socksetup () {
+  int sock, yes=1;
+  struct addrinfo hints, *servinfo, *p;
+
+	// INITIALISE their_addr
+          memset (&hints, 0, sizeof(hints));
+     	  hints.ai_family=AF_UNSPEC;
+          hints.ai_socktype=SOCK_STREAM;
+          hints.ai_flags=AI_PASSIVE; 
+       	  if(0 != getaddrinfo(NULL, PORT, &hints, &servinfo))
+            exit(1);
+
+    for(p=servinfo; p!=NULL; p=p->ai_next) {
+	if(-1==(sock=socket(p->ai_family, p->ai_socktype, p->ai_protocol)))
+	  continue;
+	if(-1==setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)))
+	  continue;
+	if(-1== bind(sock, p->ai_addr, p->ai_addrlen)){
+	  close(sock);
+	  continue;
+          }
+      break;
+      }
+  if(p==NULL)
+    exit(EXIT_FAILURE);
+
+  freeaddrinfo(servinfo);
+
+return sock;
 }
 
 void clrbuf(char* buffer){
   int i;
   for(i=0;i<strlen(buffer);i++)
-  buffer[i]='\0';
+    buffer[i]='\0';
 }
 
-
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-      return &(((struct sockaddr_in*)sa)->sin_addr);
+void updateclients(fd_set *active_set, char* buffer, int sock){
+  int i;
+  printf("Sending message back to clients\n\t%s", buffer);
+  for (i=0;i<FD_SETSIZE; i++)
+    if(FD_ISSET(i, active_set) && i != sock){
+      if(-1 == send(i, buffer, 140, 0)){
+	printf("closing %d\n", i);
+        close(i);
+        FD_CLR(i, active_set);
+      }
     }
-  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  clrbuf(buffer);
+}
+
+int getmsg (int fd, char *buffer) {
+  int numbytes;
+  numbytes= recv(fd, buffer, MAXDATASIZE-1, 0);
+  if(0 > numbytes)
+    exit(EXIT_FAILURE);
+  else if(0 == numbytes)
+    return -1;
+  else{
+    printf("recv: %s\n", buffer);
+    return 0;
+    }
 }
 
 int main(){
+  int sock, i,j, size;
+  fd_set active_set, read_set;
   struct sockaddr_storage their_addr;
-  struct addrinfo hints, *servinfo, *p;
-  int sockfd, yes=1, addr_size, new_fd, numbytes;
-  char s[INET6_ADDRSTRLEN], *buffer;
-    buffer= malloc(140*sizeof(char));
+  char *buffer = (char *)malloc(MAXDATASIZE*sizeof(char));
+  
+  sock = socksetup();
+      //  BACKLOG SET TO 1 HERE... is that ok?
+  if(0 > listen(sock, 1)) exit(EXIT_FAILURE);
+  
+  FD_ZERO(&active_set);
+  FD_SET(sock, &active_set);
 
-  FILE* wlogfp;
-wlogfp=setwlogfile("s");
-loginfo(wlogfp,"INFO", "something's going through maybe...", "now??");
-
-        // SET UP addrinfo STRUCT hints TO BE FED INTO servinfo
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-
-  if ((getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-       loginfo(wlogfp, "ERROR","something in getaddrinfo() fucked up", NULL);
-    exit(1);
-  }
-
-  // LOOP THROUGH servinfo AND BIND TO FIRST 
-  for (p=servinfo; p!=NULL; p=p->ai_next) {
-    if(-1 == (sockfd=socket(p->ai_family, p->ai_socktype,p->ai_protocol)))
-        continue;
-    if(-1 == setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)))
-        continue;
-    if(-1 == (bind(sockfd, p->ai_addr, p->ai_addrlen))){
-        close(sockfd);
-        continue;
+  while(1){
+    read_set=active_set;
+    if(0 > select(FD_SETSIZE, &read_set, NULL, NULL, NULL)){
+      exit(EXIT_FAILURE);
     }
-  break;
-  }
-if(p==NULL){
-loginfo(wlogfp, "ERROR"," failed to bind", NULL);
-exit(1);}
-        
-freeaddrinfo(servinfo); // all done with this structure
 
-if(listen(sockfd, BACKLOG)==-1) {
-loginfo(wlogfp, "ERROR","failed to listen", NULL);
-exit(1);
-}
-
-// REAP DEAD PROCESSES 
-struct sigaction sa;
-sa.sa_handler = sigchld_handler; // reap all dead processes
-sigemptyset(&sa.sa_mask);
-sa.sa_flags = SA_RESTART;
-if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-loginfo(wlogfp, "ERROR","reaping dead processes (sigaction)", NULL);
-exit(1);}
-/* 
- * (no idea how this works) 
- */
-
-loginfo(wlogfp, "INFO","waitin on connections....", NULL);
-    while(1){
-       addr_size = sizeof their_addr;
-       new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-      
-      if(new_fd==-1)
-        continue;
-      
-      inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-      loginfo(wlogfp, "INFO","got a connection!!", NULL);
-      
-      if(!fork()){
-        loginfo(wlogfp, "INFO", "closing the connection..", NULL);
-        close(sockfd);
-
-           //RECEIVING MESSAGES
-   while((numbytes = recv(new_fd, buffer, MAXDATASIZE-1, 0)) >0) {
-      buffer[numbytes]='\0';
-      loginfo(wlogfp, "INFO", "Received message:", buffer);
-
-          if (send(new_fd, buffer, 140, 0) == -1)
-            loginfo(wlogfp, "ERROR", "didnt send the message. :(", NULL);
-      clrbuf(buffer);
-}
-
-        close(new_fd);
-        exit(0);
-      }
-      close(new_fd);
+    for(i=0;i<FD_SETSIZE;i++){
+      if(FD_ISSET(i, &read_set)){
+        if(i==sock){
+	    printf("new connection! %d\n", i);
+          int new_fd;
+	  size=sizeof(their_addr); 
+	  new_fd=accept(sock, (struct sockaddr *)&their_addr, &size);
+	  if(0 > new_fd) exit(EXIT_FAILURE);
+		// MAYBE RECORD CONNECTION DETAILS HERE??
+		// see http://www.gnu.org/software/libc/manual/html_node/Server-Example.html
+		//     fprintf (stderr,
+		//         "Server: connect from host %s, port %hd.\n",
+		//          inet_ntoa (clientname.sin_addr),
+		//          ntohs (clientname.sin_port)
+		//          );
+	  FD_SET(new_fd, &active_set);
+	} else {
+	    if(0 > (j=getmsg(i, buffer))){
+	      close(i);
+	      FD_CLR(i, &active_set);
+	    }
+	    updateclients(&active_set, buffer, sock);
+          }
+	}
     }
-    
-free(buffer);
-fclose(wlogfp);
+  } 
+
+  for(i=0;i<FD_SETSIZE;i++)
+    if(FD_ISSET(i, &active_set)){
+      close(i);
+      FD_CLR(i, &active_set);
+    }
+  free(buffer);
 return 0;
 }
