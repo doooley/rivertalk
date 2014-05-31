b8b6c7c305c683de765eb4b2abe2fc7cc078a3c9
doooley <dooleyct@tcd.ie>
2014-05-31 21:10:40 +0000

    usernames added

--- src/rivertalk.c
+++ src/rivertalk.c
@@ -11,11 +11,13 @@
 
 #define PORT "4991"
 #define MAXDATASIZE 140
+#define USERNAMESIZE 15
+
 
 void clrbuf(char* buffer){
   int i;
   for(i=0;i<strlen(buffer);i++)
-  buffer[i]=' ';
+  buffer[i]='\0';
 }
 
 int socksetup (char *addr) {
@@ -46,48 +48,78 @@ int socksetup (char *addr) {
 return sock;
 }
 
-void startscreen(){
-  int row, col, my, mx;
+
+char* parsemsg(char* user, char *msg, int userlength){
+  int i;
+  char* prefix = (char*) calloc((userlength + 4),sizeof(char));
+  char* all = (char*) calloc((MAXDATASIZE + USERNAMESIZE + 4),sizeof(char));
+  int msglength=strlen(msg);
+
+   strncat(prefix, user, userlength);
+   prefix[userlength]=' ';
+   strncat(all, prefix, userlength+1);
+   strncat(all, msg, msglength);
+
+  free(prefix);
+
+return all;
+}
+
+int getuser(char* msg){
+  int i;
+  for(i=0;msg[i]!=' '; i++);
+  return i;
+}
+
+char* startscreen(){
+  int row, col, my, mx, inx, iny;
+  char *user=(char*)malloc(13*sizeof(char));
   char msg[]="rivertalk.";
 
   initscr();
-  raw();
-  noecho();
+  echo();
 
   getmaxyx(stdscr,row,col);
   my=(int)row*0.5;
-  mx=(int)(col - 5)*0.5;
+  mx=(int)(col - 10)*0.5;
+iny= my+2;
+inx=mx-7;
 
   attron(A_BOLD);
   mvprintw(my, mx, msg);
   attroff(A_BOLD);
   refresh();
-
-  getch();
+    getch();
+  mvprintw(iny, inx, "Username: ");
+  refresh();
+mvgetstr(iny, inx+ 10, user);
   clear();
   refresh();
   endwin();
+ return user;
 }
+
 
 int main(int argc, char *argv[]){
   int sockfd, numbytes, childproc;
-  int y, x;
-  char *buffer = (char*) malloc(MAXDATASIZE*sizeof(char));
+  int y, x , i , L, index;
+  char *msg, *user, *buffer = (char*) malloc((USERNAMESIZE + MAXDATASIZE + 4)*sizeof(char));
   char *addr = NULL;
     if(argc >= 2){
       addr=argv[1];
 	}
   sockfd=socksetup(addr);
 
-    startscreen(addr);
+    user=startscreen();
+    L=strlen(user);
+
 	//  SETTING UP ncurses WINDOWS
         WINDOW *in, *out;
         initscr();
         cbreak();
 	echo();
         in = newwin(4, 0, LINES-4, 0);
-          box(in, 0, 0);
-        out = newwin(10, 0, 0, 0);
+        out = newwin(LINES-4, 0, 0, 0);
 	scrollok(in, TRUE);
 	scrollok(out, TRUE);
 
@@ -96,19 +128,27 @@ int main(int argc, char *argv[]){
 
   if(childproc >= 0){
     if(childproc > 0){	 //RECEIVING MESSAGES
-      while((numbytes = recv(sockfd, buffer, MAXDATASIZE-1, 0)) >0) {
-		if(0 < strlen(buffer))
-            wprintw(out, "%s\n", buffer);
+      while((numbytes = recv(sockfd, buffer, (USERNAMESIZE + MAXDATASIZE + 4), 0)) >0) {
+		if(0 < strlen(buffer)){
+                  index=getuser(buffer);
+                  wattron(out, A_BOLD);
+                  for(i=0;i<index;i++)
+                    wprintw(out, "%c", buffer[i]);
+                  wattroff(out, A_BOLD);
+                  wprintw(out, "%s\n", buffer + index);
+                }
 	    wrefresh(out);
 	    wrefresh(in);
-	    wmove(in, 1, 1);
+	    wmove(in, 0, 0);
         }
       } else	 	//SENDING MESSAGES
     while(1){
-        mvwgetstr(in,1, 1, buffer);
-	wmove(in, 1, 1);
-        wclrtoeol(in);
-         if (send(sockfd, buffer, 140, 0) == -1)
+        mvwgetstr(in,0, 0, buffer);
+	wmove(in, 0, 0);
+        werase(in);
+        wrefresh(in);
+        msg=parsemsg(user, buffer, L);
+         if (send(sockfd, msg, MAXDATASIZE + USERNAMESIZE + 4, 0) == -1)
 		close(sockfd);
          }
   }
